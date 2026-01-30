373db8e53484a1b3bd94143537f8e4c9f044b2cf
asil <asil9802mum@gmail.com>
2026-01-30 22:07:40 +0100

    i dont undestand shit

--- src/main/java/org/cmd/udpsender/UDP.java
+++ src/main/java/org/cmd/udpsender/UDP.java
@@ -20,25 +20,40 @@ public class UDP {
     }
 
     private static void createConnection() throws IOException {
-        var hostAddress = InetAddress.getByName("localhost");
-        var inetSocket = new InetSocketAddress(hostAddress, 42069);
-        var dataSocket = new DatagramSocket(42070);
-        dataSocket.connect(inetSocket);
+        // var inetSocket = new InetSocketAddress(InetAddress.getByName("localhost"), 42069);
+        byte[] buffer = new byte[20];
+        InetAddress laddr = InetAddress.getByName("localhost");
+        // System.out.println(laddr.getHostAddress());
+        // System.out.println(laddr.getHostName());
+        var dataSocket = new DatagramSocket();
+        // // dataSocket.bind(laddr);
+        // System.out.println(dataSocket.isBound());
+        // System.out.println(dataSocket.isConnected());
+        // System.out.println(dataSocket.getLocalPort());
+        // System.out.println(dataSocket.getInetAddress());
 
         var bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
             System.out.println(">");
-            String line;
-            try {
+            String line = "";
+            try {       
                 line = bufferedReader.readLine();
             } catch (IOException e) {
-                line = e.getMessage();
                 // TODO Auto-generated catch block
                 e.printStackTrace();
+                System.out.println(e.getMessage());
+
             }
-            var datagramPacket = new DatagramPacket(line.getBytes(), line.getBytes().length);
-            dataSocket.send(datagramPacket);
+            var datagramPacket = new DatagramPacket(line.getBytes(), line.getBytes().length, laddr, 42069);
+            try {
+
+                dataSocket.send(datagramPacket);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+                // lol do nothing
+            } 
             
         }
 
