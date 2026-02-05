d1d42ede452a7681beaf04083aa62a2255885b2f
asil <asil9802mum@gmail.com>
2026-02-05 13:52:00 +0100

    moved to gradle commands + junit

---
+++ src/test/java/org/internal/request/TestRequest.java
@@ -0,0 +1,12 @@
+package org.internal.request;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestRequest {
+
+    @Test
+    public void testRequestLineParse() {
+        assertEquals("TheTestagen", "TheTestagen");
+    }
+}
